71937c20bc23e410f8a5f305d4dd52e46d15ae1a
KarrimorRifle <samuelto888@yahoo.co.uk>
2022-10-30 23:34:27 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express()
 const path = require('path');
-const port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname + '/public')));
 
 app.get('/', (req,res) => {
